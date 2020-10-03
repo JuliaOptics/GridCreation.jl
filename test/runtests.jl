6de2135d7dc942ac0813e04b8fbf529bfe3849d2
Brandon <brandondube@gmail.com>
2020-10-03 16:47:54 -0700

    test: restructure tests, seems have to be inside of a testset to trigger on CI?

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,42 +1,51 @@
 using Test
 using GridCreation
 
-xfft, yfft = mkCartVecs(1., 4, center=CFFT);
 
- # test zero in the correct place
-@test xfft[3] ≈ 0
-@test yfft[3] ≈ 0
 
-# test spacing correct
-@test xfft[1] ≈ -2
-@test yfft[1] ≈ -2
+@testset "fft alignment" begin
+    xfft, yfft = mkCartVecs(1., 4, center=CFFT);
+    # alignment
+    @test xfft[3] ≈ 0
+    @test yfft[3] ≈ 0
 
-# test inter-sample centering is properly aligned
-xinter, yinter = mkCartVecs(1., 4, center=CInterSample);
-@test xinter[2] ≈ -0.5
-@test xinter[3] ≈  0.5
-@test yinter[2] ≈ -0.5
-@test yinter[3] ≈  0.5
+    # spacing
+    @test xfft[1] ≈ -2
+    @test yfft[1] ≈ -2
+end
 
+
+@testset "intersample alignment" begin
+    xinter, yinter = mkCartVecs(1., 4, center=CInterSample);
+    @test xinter[2] ≈ -0.5
+    @test xinter[3] ≈  0.5
+    @test yinter[2] ≈ -0.5
+    @test yinter[3] ≈  0.5
+end
+
+@testset "first sample centering" begin
 # test sample index centering is properly aligned
-x0, y0 = mkCartVecs(1., 4, center=1);
-@test x0[1] ≈ 0
-@test y0[1] ≈ 0
+    x0, y0 = mkCartVecs(1., 4, center=1);
+    @test x0[1] ≈ 0
+    @test y0[1] ≈ 0
+end
 
-# test polar properly converts
-ρ, θ = cartVecsToPolarGrid(xfft, yfft);
-@test ρ[3,3] ≈ 0
-@test ρ[3,2] ≈ 1
-@test ρ[2,3] ≈ 1
-@test ρ[3,4] ≈ 1
-@test ρ[4,3] ≈ 1
-@test ρ[4,4] ≈ √2
-@test ρ[2,2] ≈ √2
-@test ρ[2,4] ≈ √2
-@test ρ[4,2] ≈ √2
-@test θ[3,3] ≈ 0
-@test θ[3,4] ≈ 0
-@test θ[3,2] ≈ π
-@test θ[3,1] ≈ π
-@test θ[2,3] ≈ -π/2
-@test θ[4,3] ≈ π/2
+@testset "polar grid" begin
+    xfft, yfft = mkCartVecs(1., 4, center=CFFT);
+    ρ, θ = cartVecsToPolarGrid(xfft, yfft);
+    @test ρ[3,3] ≈ 0
+    @test ρ[3,2] ≈ 1
+    @test ρ[2,3] ≈ 1
+    @test ρ[3,4] ≈ 1
+    @test ρ[4,3] ≈ 1
+    @test ρ[4,4] ≈ √2
+    @test ρ[2,2] ≈ √2
+    @test ρ[2,4] ≈ √2
+    @test ρ[4,2] ≈ √2
+    @test θ[3,3] ≈ 0
+    @test θ[3,4] ≈ 0
+    @test θ[3,2] ≈ π
+    @test θ[3,1] ≈ π
+    @test θ[2,3] ≈ -π/2
+    @test θ[4,3] ≈ π/2
+end
